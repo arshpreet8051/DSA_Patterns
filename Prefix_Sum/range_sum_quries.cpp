24527cf037ac4c529836c2d593a7b8b28d5e3b43
Arshpreet_Singh <arshpreet8051@gmail.com>
2025-04-21 23:51:37 +0530

    Range Sum Quries

---
+++ Prefix_Sum/range_sum_quries.cpp
@@ -0,0 +1,26 @@
+/* Leetcode 303 */
+class NumArray {
+    public:
+        vector<int> prefix;
+        NumArray(vector<int>& nums) {
+            
+            int n = nums.size();
+            prefix.resize(n);
+            int sum = 0;
+            for(int i = 0;i<n;i++){
+                sum+=nums[i];
+                prefix[i]= sum;
+            }
+        }
+        
+        int sumRange(int left, int right) {
+            if(left==0){return prefix[right];}
+            return prefix[right] - prefix[left-1];
+        }
+    };
+    
+    /**
+     * Your NumArray object will be instantiated and called as such:
+     * NumArray* obj = new NumArray(nums);
+     * int param_1 = obj->sumRange(left,right);
+     */
